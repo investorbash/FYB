1af4345e24eed3d4fbef561c5afe7a59da9342fa
Bashorun Olaide <officialinvestorbash@hotmail.com>
2024-06-05 13:25:02 +0100

    Version 1.0

---
+++ src/Certificate.js
@@ -0,0 +1,68 @@
+// Certificate.js
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
+
+const Certificate = ({
+    nickname,
+    socialMediaHandle,
+    dob,
+    relationshipStatus,
+    bestColor,
+    stateOfOrigin,
+    hobbies,
+    classCrush,
+    favoriteCourse,
+    toughestLevel,
+    hardestCourse,
+    favoriteQuote,
+    whatWillUMiss,
+    user_image
+}) => {
+    const certificateRef = useRef();
+
+    const handleDownloadImage = async () => {
+        const element = certificateRef.current;
+        const canvas = await html2canvas(element);
+        const data = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = `${nickname}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <div>
+            <div className='container' ref={certificateRef} style={{ padding: '20px', border: '1px solid #000', textAlign: 'center', backgroundColor:  bestColor ||'yellow' }}>
+                <h1>FINAL YEAR BRETHREN</h1>
+                <h3>PERSONALTY OF THE DAY</h3>
+                <div className='row'>
+                    <div className="col-sm-6 col-md-6 w-50 h-50" >
+                        <img src={user_image} alt="User"  style={{ width: '100%', height: '100%', borderRadius: '5%'}} />  
+                        {/* style={{ width: '150px', height: '150px', borderRadius: '50%' }} */}
+                    </div>
+                    <div className="col-sm-6 col-md-6">
+                        <p style={{fontWeight: '1.2em',}}><strong>Nickname:</strong> {nickname}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Social Media Handle:</strong> {socialMediaHandle}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Date of Birth:</strong> {dob}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Relationship Status:</strong> {relationshipStatus}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>State of Origin:</strong> {stateOfOrigin}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Hobbies:</strong> {hobbies}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Class Crush:</strong> {classCrush}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Favorite Course:</strong> {favoriteCourse}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Toughest Level:</strong> {toughestLevel}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Hardest Course:</strong> {hardestCourse}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>Favorite Quote:</strong> {favoriteQuote}</p>
+                        <p style={{fontWeight: '1.2em',}}><strong>What Will You Miss:</strong> {whatWillUMiss}</p>
+                        <br />
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <button onClick={handleDownloadImage}>Download as Image</button>
+        </div>
+    );
+};
+
+export default Certificate;
---
+++ src/CertificateForm.js
@@ -0,0 +1,134 @@
+// CertificateForm.js
+import React, { useState } from 'react';
+import Certificate from './Certificate';
+
+const CertificateForm = () => {
+    const [formData, setFormData] = useState({
+        nickname: '',
+        socialMediaHandle: '',
+        dob: '',
+        relationshipStatus: '',
+        bestColor: '',
+        stateOfOrigin: '',
+        hobbies: '',
+        classCrush: '',
+        favoriteCourse: '',
+        toughestLevel: '',
+        hardestCourse: '',
+        favoriteQuote: '',
+        whatWillUMiss: '',
+        user_image: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setFormData({
+                    ...formData,
+                    user_image: reader.result
+                });
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Generate certificate after form submission
+        setFormData({ ...formData });
+    };
+
+    return (
+        <div>
+            <h1>Final Year Brethren</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Nickname: </label>
+                    <input type="text" name="nickname" value={formData.nickname} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Social Media Handle: </label>
+                    <input type="text" name="socialMediaHandle" value={formData.socialMediaHandle} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Date of Birth: </label>
+                    <input type="date" name="dob" value={formData.dob} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Relationship Status: </label>
+                    <input type="text" name="relationshipStatus" value={formData.relationshipStatus} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Best Color: </label>
+                    <input type="color" name="bestColor" value={formData.bestColor} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>State of Origin: </label>
+                    <input type="text" name="stateOfOrigin" value={formData.stateOfOrigin} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Hobbies: </label>
+                    <input type="text" name="hobbies" value={formData.hobbies} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Class Crush: </label>
+                    <input type="text" name="classCrush" value={formData.classCrush} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Favorite Course: </label>
+                    <input type="text" name="favoriteCourse" value={formData.favoriteCourse} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Toughest Level: </label>
+                    <input type="text" name="toughestLevel" value={formData.toughestLevel} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Hardest Course: </label>
+                    <input type="text" name="hardestCourse" value={formData.hardestCourse} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>Favorite Quote: </label>
+                    <input type="text" name="favoriteQuote" value={formData.favoriteQuote} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>What Will You Miss: </label>
+                    <input type="text" name="whatWillUMiss" value={formData.whatWillUMiss} onChange={handleChange} required />
+                </div>
+                <br />
+                <div>
+                    <label>User Image: </label>
+                    <input type="file" accept="image/*" onChange={handleImageUpload} required />
+                </div>
+                {/* <button type="submit">Generate Certificate</button> */}
+            </form>
+
+            {formData.nickname && (
+                <Certificate {...formData} />
+            )}
+        </div>
+    );
+};
+
+export default CertificateForm;
